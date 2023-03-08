4d87da36c2b7d8243c9326856a098edddc134d24
Leandro Mateus <leandroyow@gmail.com>
2023-03-08 10:08:46 -0300

    added and working contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,43 +1,176 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
-    return (<div>
-        <div style={{ color: "white" }} className="d-flex flex-row mt-5 pt-5 pb-5 mb-3">
-            <h3 className="pe-3 ps-2">Contato</h3>
-            <hr />
+  const form = useRef();
+  const [IsEmailSent, setIsEmailSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_03qmxbt",
+        "template_h1ujwkk",
+        form.current,
+        "XRn9dbxzV1qrTmXhv"
+      )
+      .then(
+        (result) => {
+          setIsEmailSent(true);
+        },
+        (error) => {
+          setIsEmailSent(false);
+        }
+      );
+  };
+
+  const toastTrigger = document.getElementById("liveToastBtn");
+  const toastLiveExample = document.getElementById("liveToast");
+
+  if (toastTrigger) {
+    toastTrigger.addEventListener("click", () => {
+
+      const emailField = document.querySelector("#inputEmail");
+      const messageField = document.querySelector("#inputMessage");
+
+      if (IsEmailSent && emailField.value !== "" && messageField.value !== "") {
+        const toast = new bootstrap.Toast(toastLiveExample);
+        toast.show();
+      }
+    });
+  }
+
+  return (
+    <div>
+      <div
+        style={{ color: "white" }}
+        className="d-flex flex-row mt-5 pt-5 pb-5 mb-3"
+      >
+        <h3 className="pe-3 ps-2">Contato</h3>
+        <hr />
+      </div>
+
+      <div className="toast-container position-fixed bottom-0 end-0 p-3">
+        <div
+          id="liveToast"
+          className="toast"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          <div className="toast-header">
+            <i className="fa-solid fa-envelope-circle-check rounded me-2"></i>
+            <strong className="me-auto">Mensagem enviada com sucesso!</strong>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
+            <div style={{color:"gray !important"}} className="toast-body">
+              Em breve entrarei em contato.
+            </div>
         </div>
+      </div>
 
-        <div className="d-flex justify-content-center gap-5">
-
-        <a href="https://github.com/leandroyow"
-           target={"_blank"}>
-        <i style={{color:"gray"}} className="fa-brands fa-github contact-icon"></i>
+      <div className="d-flex justify-content-center gap-5">
+        <a href="https://github.com/leandroyow" target={"_blank"}>
+          <i
+            style={{ color: "gray" }}
+            className="fa-brands fa-github contact-icon"
+          ></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/leandro-mateus-50a960238/"
-           target={"_blank"} >
-        <i style={{color:"#0a66c2"}} class="fa-brands fa-linkedin contact-icon"></i>
+        <a
+          href="https://www.linkedin.com/in/leandro-mateus-50a960238/"
+          target={"_blank"}
+        >
+          <i
+            style={{ color: "#0a66c2" }}
+            className="fa-brands fa-linkedin contact-icon"
+          ></i>
         </a>
+      </div>
 
-        </div>
+      <div
+        style={{ width: "100%" }}
+        className="d-flex flex-column align-items-center justify-content-center"
+      >
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          action="submit"
+          className="mt-5 ms-5 me-5 mail-form"
+        >
+          <h1 className="text-center" style={{color:"white"}}></h1>
+          <div className="mb-3">
+            <label
+              style={{ color: "rgb(210, 210, 210)" }}
+              htmlFor="inputEmail"
+              className="form-label"
+            >
+              Nome
+            </label>
+            <input
+              type="text"
+              placeholder="Digite seu nome"
+              className="form-control"
+              name="user_name"
+              id="inputName"
+              aria-describedby="emailHelp"
+            />
+          </div>
 
-        <div style={{width: "100%"}} className="d-flex flex-column align-items-center justify-content-center">
+          <div className="mb-3">
+            <label
+              style={{ color: "rgb(210, 210, 210)" }}
+              htmlFor="inputEmail"
+              className="form-label"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Digite seu email"
+              className="form-control"
+              name="user_email"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              required
+            />
+          </div>
 
-
-        <form action="submit" className="mt-5 ms-5 me-5 mail-form">
-            <div class="mb-3">
-                <label style={{color:"rgb(210, 210, 210)"}} for="inputEmail" class="form-label">Email</label>
-                <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
-            </div>
-            <div class="mb-3">
-                <label style={{color:"rgb(210, 210, 210)"}} autoComplete="on"  for="inputMessage" class="form-label">Mensagem</label>
-                <textarea style={{height:250}} autoComplete="on"  className="form-control" id="inputMessage" />
-            </div>
-            <div className="w-100 text-center">
-            <button style={{width:"50%"}} type="submit" className="mt-2 btn btn-outline-success">Enviar</button>
-            </div>
+          <div className="mb-3">
+            <label
+              style={{ color: "rgb(210, 210, 210)" }}
+              autoComplete="on"
+              htmlFor="inputMessage"
+              className="form-label"
+            >
+              Mensagem
+            </label>
+            <textarea
+              style={{ height: 250 }}
+              name="message"
+              placeholder="..."
+              className="form-control"
+              id="inputMessage"
+              required
+            />
+          </div>
+          <div className="w-100 text-center">
+            <input
+              style={{ width: "50%" }}
+              type="submit"
+              value="Enviar"
+              className="btn btn-outline-success"
+              id="liveToastBtn"
+            />
+          </div>
         </form>
-
-
-        </div>
+      </div>
     </div>
-    )
+  );
 }
--- src/components/stacks-section.jsx
+++ src/components/stacks-section.jsx
@@ -7,12 +7,12 @@ export default function Stacks() {
             </div>
             <p style={{color: "rgb(210, 210, 210)"}} className="fw-light ps-5 pe-5 mt-5">Eu amo o aprendizado baseado em projetos, ter que aprender enquanto faço e construo coisas não é fácil, mas foi algo que me atraiu muito e sempre foi gratificante. Aprendi e estou aprendendo e aprimorando habilidades e ferramentas de trabalho todos os dias:</p>
             <div className="ms-5 me-5 pt-5 d-flex justify-content-evenly">
-            <i style={{color: "#f54d27"}} class="fa-brands fa-git-alt stack"></i>
-            <i style={{color:"#f26528"}} class="fa-brands fa-html5 stack"></i>
-            <i style={{color:"#264de4"}} class="fa-brands fa-css3-alt stack"></i>
-            <i style={{color:"#edd62e"}} class="fa-brands fa-js stack"></i>
-            <i style={{color:"#61dafb"}} class="fa-brands fa-react stack"></i>
-            <i style={{color:"#7310f5"}} class="fa-brands fa-bootstrap stack"></i>
+            <i style={{color: "#f54d27"}} className="fa-brands fa-git-alt stack"></i>
+            <i style={{color:"#f26528"}} className="fa-brands fa-html5 stack"></i>
+            <i style={{color:"#264de4"}} className="fa-brands fa-css3-alt stack"></i>
+            <i style={{color:"#edd62e"}} className="fa-brands fa-js stack"></i>
+            <i style={{color:"#61dafb"}} className="fa-brands fa-react stack"></i>
+            <i style={{color:"#7310f5"}} className="fa-brands fa-bootstrap stack"></i>
             </div>
         </div>
         )
